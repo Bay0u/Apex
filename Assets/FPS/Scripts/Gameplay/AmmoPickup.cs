9ad32b4a0490f842e91377b4d46d4e480d6c08ec
Reighon <79049309+Bay0u@users.noreply.github.com>
2021-12-28 02:24:23 +0200

    Ammo update

--- Assets/FPS/Scripts/Gameplay/AmmoPickup.cs
+++ Assets/FPS/Scripts/Gameplay/AmmoPickup.cs
@@ -5,29 +5,81 @@ namespace Unity.FPS.Gameplay
 {
     public class AmmoPickup : Pickup
     {
-        [Tooltip("Weapon those bullets are for")]
-        public WeaponController Weapon;
-
         [Tooltip("Number of bullets the player gets")]
-        public int BulletCount = 30;
+        public int BulletCount = 50;
 
         protected override void OnPicked(PlayerCharacterController byPlayer)
         {
             PlayerWeaponsManager playerWeaponsManager = byPlayer.GetComponent<PlayerWeaponsManager>();
+
             if (playerWeaponsManager)
             {
-                WeaponController weapon = playerWeaponsManager.HasWeapon(Weapon);
-                if (weapon != null && Input.GetButton(GameConstants.k_ButtonPickup))
+                WeaponController weapon1 = playerWeaponsManager.m_WeaponSlots[0];
+                WeaponController weapon2 = playerWeaponsManager.m_WeaponSlots[1];
+                if (this.gameObject.name == "PrimaryAmmo")
                 {
-                    weapon.AddCarriablePhysicalBullets(BulletCount);
+                    if (weapon1.WeaponName == "Sniper" || weapon1.WeaponName == "Shotgun" || weapon1.WeaponName == "Blaster")
+                    {
+                        if (weapon1 != null && Input.GetButton(GameConstants.k_ButtonPickup))
+                        {
+                            weapon1.AddCarriablePhysicalBullets(BulletCount);
 
-                    AmmoPickupEvent evt = Events.AmmoPickupEvent;
-                    evt.Weapon = weapon;
-                    EventManager.Broadcast(evt);
-                    PlayPickupFeedback();
-                    Debug.Log(weapon.GetCurrentAmmo());
-                    Destroy(gameObject);
+                            AmmoPickupEvent evt = Events.AmmoPickupEvent;
+                            evt.Weapon = weapon1;
+                            EventManager.Broadcast(evt);
+                            PlayPickupFeedback();
+                            Debug.Log(weapon1.GetCurrentAmmo());
+                            Destroy(gameObject);
+                        }
+                    }
+                    if (weapon2.WeaponName == "Sniper" || weapon2.WeaponName == "Shotgun" || weapon2.WeaponName == "Blaster")
+                    {
+                        if (weapon2 != null && Input.GetButton(GameConstants.k_ButtonPickup))
+                        {
+                            weapon2.AddCarriablePhysicalBullets(BulletCount);
+
+                            AmmoPickupEvent evt = Events.AmmoPickupEvent;
+                            evt.Weapon = weapon2;
+                            EventManager.Broadcast(evt);
+                            PlayPickupFeedback();
+                            Debug.Log(weapon2.GetCurrentAmmo());
+                            Destroy(gameObject);
+                        }
+                    }
                 }
+                if (this.gameObject.name == "SecondaryAmmo")
+                {
+                    if (weapon1.WeaponName == "GLauncher" || weapon1.WeaponName == "FLauncher")
+                    {
+                        if (weapon1 != null && Input.GetButton(GameConstants.k_ButtonPickup))
+                        {
+                            weapon1.AddCarriablePhysicalBullets(BulletCount);
+
+                            AmmoPickupEvent evt = Events.AmmoPickupEvent;
+                            evt.Weapon = weapon1;
+                            EventManager.Broadcast(evt);
+                            PlayPickupFeedback();
+                            Debug.Log(weapon1.GetCurrentAmmo());
+                            Destroy(gameObject);
+                        }
+                    }
+                    if (weapon2.WeaponName == "GLauncher" || weapon2.WeaponName == "FLauncher")
+                    {
+                        if (weapon2 != null && Input.GetButton(GameConstants.k_ButtonPickup))
+                        {
+                            weapon2.AddCarriablePhysicalBullets(BulletCount);
+
+                            AmmoPickupEvent evt = Events.AmmoPickupEvent;
+                            evt.Weapon = weapon2;
+                            EventManager.Broadcast(evt);
+                            PlayPickupFeedback();
+                            Debug.Log(weapon2.GetCurrentAmmo());
+                            Destroy(gameObject);
+                        }
+                    }
+                }
+
+
             }
         }
     }
