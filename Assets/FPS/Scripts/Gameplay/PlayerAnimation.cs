89ff9322b4b3dddfc1c6c1fc7591a0110ea079c2
Eiad21 <eiadessam1@gmail.com>
2021-12-29 20:21:46 +0200

    Added pickup animation

---
+++ Assets/FPS/Scripts/Gameplay/PlayerAnimation.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Unity.FPS.Gameplay
+{
+    public class PlayerAnimation : MonoBehaviour
+    {
+        Animator animator;
+        // Start is called before the first frame update
+        void Start()
+        {
+            animator = GetComponent<Animator>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                animator.SetTrigger("Pick");
+               // animator.SetBool("Pick", false);
+            }
+            if (PlayerCharacterController.IsDead)
+            {
+                print("mango");
+                animator.SetTrigger("Die");
+            }
+
+
+        }
+    }
+}
